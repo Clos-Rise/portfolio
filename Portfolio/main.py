72079d07b4f0bfd15b9bcdc26844431a7e30dfe0
Clos-Rise <95033598+Clos-Rise@users.noreply.github.com>
2024-06-09 14:27:41 +0500

    Update main.py

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -5,13 +5,11 @@ from flask import Flask, render_template,request, redirect
 
 app = Flask(__name__)
 
-#Запуск страницы с контентом
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-#Динамичные скиллы
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
